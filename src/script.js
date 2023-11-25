d8ef77bb3596c394ef44544f9f10a2a5e99f6f0b
Aitishnitsa <aitishnitsa@gmail.com>
2023-11-25 19:55:04 +0200

    start

---
+++ src/script.js
@@ -0,0 +1,36 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+const list = document.querySelector("#birds-list");
+
+const createProductElement = (array) => {
+    array.forEach(item => {        
+      const section = document.createElement("section");
+      const img = document.createElement("img");
+      const name = document.createElement("h3");
+      const description = document.createElement("p");
+  
+      section.classList.add("bg-white", "p-4", "rounded-md", "shadow-md", "flex", "flex-col");
+      name.classList.add("text-lg", "font-semibold", "mb-2");
+      img.classList.add("w-full", "h-64", "object-contain", "mb-4", "rounded-md", "flex-shrink-0");
+      description.classList.add("text-gray-600");
+  
+      name.innerHTML = item.name;
+      img.src = item.imageUrl;
+      description.innerHTML = item.description;
+  
+      section.append(img, name, description);
+      list.appendChild(section);
+    });
+  }
+  
+  fetch('./birds.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(json => {
+      createProductElement(json);
+    });
